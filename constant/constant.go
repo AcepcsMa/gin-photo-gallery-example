796d9b2fd728dc58f4fd176bdde0e9093d3fe072
haoxiangma <mahomehi@126.com>
2019-02-22 16:40:34 -0800

    constants

---
+++ constant/constant.go
@@ -0,0 +1,44 @@
+package constant
+
+const (
+
+	// JWT constants
+	JWT_SECRET 			= "JWT_SECRET"
+	JWT 				= "jwt"
+	JWT_EXP_MINUTE 		= 30
+	PHOTO_STORAGE_ADMIN = "admin"
+
+	// Server constants
+	SERVER_PORT = "SERVER_PORT"
+	PAGE_SIZE 	= 20
+
+	// DB constants
+	DB_CONNECT 	= "%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local"
+	DB_TYPE 	= "DB_TYPE"
+	DB_HOST 	= "DB_HOST"
+	DB_PORT 	= "DB_PORT"
+	DB_USER 	= "DB_USER"
+	DB_PWD 		= "DB_PWD"
+	DB_NAME 	= "DB_NAME"
+
+	// Redis constants
+	REDIS_HOST = "REDIS_HOST"
+	REDIS_PORT = "REDIS_PORT"
+
+	// Auth constants
+	COOKIE_MAX_AGE 	= 1800
+	LOGIN_MAX_AGE 	= 1800
+	LOGIN_USER 		= "LOGIN_"
+
+	// COS constants
+	COS_BUCKET_NAME = "COS_BUCKET"
+	COS_APP_ID 		= "COS_APP_ID"
+	COS_REGION 		= "COS_REGION"
+	COS_SECRET_ID 	= "COS_SECRET_ID"
+	COS_SECRET_KEY 	= "COS_SECRET_KEY"
+
+	// Callback constants
+	URL_UPDATE_CHANNEL 		= "PHOTO_URL_UPDATE"
+	PHOTO_UPDATE_ID_FORMAT 	= "photo-%d"
+	PHOTO_DELETE_CHANNEL 	= "PHOTO_DELETE"
+)
---
+++ constant/resp_code.go
@@ -0,0 +1,74 @@
+package constant
+
+const (
+
+	// User related responses
+	USER_ALREADY_EXIST = 1001
+	USER_ADD_SUCCESS = 1002
+	USER_AUTH_SUCCESS = 1003
+	USER_AUTH_ERROR = 1004
+	USER_AUTH_TIMEOUT = 1005
+	USER_SIGNOUT_SUCCESS = 1006
+
+	// JWT related responses
+	JWT_GENERATION_ERROR = 2001
+	JWT_MISSING_ERROR = 2002
+
+	// Bucket related responses
+	BUCKET_ALREADY_EXIST = 3001
+	BUCKET_ADD_SUCCESS = 3002
+	BUCKET_NOT_EXIST = 3003
+	BUCKET_DELETE_SUCCESS = 3004
+	BUCKET_UPDATE_SUCCESS = 3005
+	BUCKET_GET_SUCCESS = 3006
+
+	// Photo related responses
+	PHOTO_ALREADY_EXIST = 4001
+	PHOTO_ADD_IN_PROCESS = 4002
+	PHOTO_UPLOAD_SUCCESS = 4003
+	PHOTO_UPLOAD_ERROR = 4004
+	PHOTO_NOT_EXIST = 4005
+
+	// Internal server responses
+	INTERNAL_SERVER_ERROR = 5001
+	PAGINATION_SUCCESS = 8001
+	INVALID_PARAMS = 9001
+
+)
+
+var Message map[int]string
+
+// Init the message map.
+func init() {
+	Message = make(map[int]string)
+	Message[INVALID_PARAMS] 		= "Invalid parameters."
+	Message[USER_ALREADY_EXIST] 	= "User already exists."
+	Message[USER_ADD_SUCCESS] 		= "Add user success."
+	Message[USER_AUTH_SUCCESS] 		= "User authentication success."
+	Message[USER_AUTH_ERROR] 		= "User authentication fail."
+	Message[USER_AUTH_TIMEOUT] 		= "User authentication timeout."
+	Message[USER_SIGNOUT_SUCCESS] 	= "User sign out success."
+	Message[JWT_GENERATION_ERROR] 	= "JWT generation fail."
+	Message[JWT_MISSING_ERROR] 		= "JWT is missing."
+	Message[INTERNAL_SERVER_ERROR] 	= "Internal server error."
+	Message[BUCKET_ALREADY_EXIST] 	= "Bucket already exists."
+	Message[BUCKET_ADD_SUCCESS] 	= "Add bucket success."
+	Message[BUCKET_NOT_EXIST]		 = "Bucket does not exist."
+	Message[BUCKET_DELETE_SUCCESS] 	= "Delete bucket success."
+	Message[BUCKET_UPDATE_SUCCESS] 	= "Update Bucket success."
+	Message[BUCKET_GET_SUCCESS] 	= "Get Bucket success."
+	Message[PHOTO_ALREADY_EXIST] 	= "Photo already exists."
+	Message[PHOTO_ADD_IN_PROCESS] 	= "Adding photo is in process."
+	Message[PHOTO_UPLOAD_SUCCESS] 	= "Photo upload success."
+	Message[PHOTO_UPLOAD_ERROR] 	= "Photo upload error."
+	Message[PHOTO_NOT_EXIST] 		= "Photo does not exist."
+}
+
+// Translate a response code to a detailed message.
+func GetMessage(code int) string {
+	msg, ok := Message[code]
+	if ok {
+		return msg
+	}
+	return ""
+}
