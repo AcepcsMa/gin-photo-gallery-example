e77c9bbd79ae1b3d740b7ebeef6a4bb463fa32fc
haoxiangma <mahomehi@126.com>
2019-03-15 18:03:46 -0700

    zap logging

---
+++ utils/logging.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var AppLogger *zap.Logger
+
+func init() {
+	w := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "logs/app.log",
+		MaxSize:    100,
+		MaxBackups: 3,
+		MaxAge:     1,
+	})
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		w,
+		zap.InfoLevel,
+	)
+
+	caller := zap.AddCaller()
+
+	AppLogger = zap.New(core, caller)
+}
